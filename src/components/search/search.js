eeab56aced2c60ce5d739e40535830beeccbcc39
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-09-20 15:00:38 -0300

    search

---
+++ src/components/search/search.js
@@ -0,0 +1,11 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = () => {
+    return (
+        <AsyncPaginate
+        placeholder="Search for city"
+        />
+    )
+}
+
+export default Search;
