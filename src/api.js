2201abc008662d5e5d13b8cdc3e12a47290bdcd1
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-09-20 15:20:56 -0300

    api

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '60279d5781msh14c01f8f47baaadp183c13jsna017b3e06bb9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = () => {
+const Search = ({onSearchChange}) => {
+    const [search, setSearch] = useState(null);
+    const handleOnSearchChange = (searchData)=>{
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    const loadOptions = (inputValue) =>{
+        return fetch(`${GEO_API_URL}/cities?` ,geoApiOptions)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err))
+
+    }
     return (
         <AsyncPaginate
         placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnSearchChange}
+        loadOptions={loadOptions}
         />
     )
 }
